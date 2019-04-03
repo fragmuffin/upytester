dc163b00a72929132d233d0dd5855fa380f39fa5
Peter Boin <Peter.Boin@gmail.com>
2019-04-04 08:27:53 +1100

    metadata

---
+++ src/upytester/__init__.py
@@ -0,0 +1,20 @@
+# ------- Package Meta Data
+__version__ = "0.1.dev1"
+
+__title__ = 'upytester'
+__description__ = 'Hardware test environment using MicroPython hardware as an interface'
+__url__ = 'https://github.com/fragmuffin/upytester'
+
+__author__ = 'Peter Boin'
+__email__ = 'peter.boin+upytester@gmail.com'
+
+__license__ = 'MIT'
+
+__keywords__ = ['micropython', 'pyboard', 'testing', 'unittest', 'hardware']
+
+# ------- Imports
+__all__ = [
+    'PyBoard',
+]
+
+from .pyboard.device import PyBoard
