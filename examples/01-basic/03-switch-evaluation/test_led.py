efa99555c3faf2f2b2f038e2823338a63364916b
Peter Boin <Peter.Boin@gmail.com>
2019-04-13 00:01:42 +1000

    switch test

---
+++ examples/01-basic/03-switch-evaluation/test_led.py
@@ -0,0 +1,37 @@
+import time
+import unittest
+import upytester
+
+
+# ------------ Bench Environment ------------
+class Switch(object):
+    def __init__(self, device):
+        self.device = device
+
+    @property
+    def value(self):
+        receiver = self.device.get_switch()
+        response = receiver()
+        return response['value']
+
+
+class BenchTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pyb_a = upytester.project.get_device('pyb_a')
+        cls.switch = Switch(device=cls.pyb_a)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.pyb_a.close()
+
+
+# ------------ Tests ------------
+
+class SwitchTest(BenchTest):
+
+    def test_switch_pressed(self):
+        """
+        Turn red LED on for 500ms asynchronously
+        """
+        self.assertTrue(self.switch.value)
