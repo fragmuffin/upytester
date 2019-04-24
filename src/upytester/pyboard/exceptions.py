9c2c6a9ac97955e4361a3eb3a186ef0d72c3294b
Peter Boin <Peter.Boin@gmail.com>
2019-04-24 18:10:50 +1000

    Exception docs

--- src/upytester/pyboard/exceptions.py
+++ src/upytester/pyboard/exceptions.py
@@ -2,4 +2,9 @@ class ResponseTimeoutException(Exception):
     """Raised when no 'ok' response is received from the pyboard"""
 
 class PyBoardError(Exception):
-    """Raised in liue of an exception raised on a remote PyBoard"""
+    """
+    Raised in liue of an exception raised on a remote PyBoard
+
+    See the example ":ref:`examples.basic.remote-exception`" for more
+    details.
+    """
