0feaa56f2eac9352fd7ced9b9e52c233e76ac30e
Peter Boin <peter@allumeenergy.com.au>
2019-11-12 14:41:16 +1100

    from upyt.cmd import remote

--- src/upytester/content/sd/lib/upyt/cmd/__init__.py
+++ src/upytester/content/sd/lib/upyt/cmd/__init__.py
@@ -3,10 +3,11 @@ __all__ = [
     'interpret',
     'get_sender',
     'send',
+    'remote',
 ]
 
 
-from .mapping import instruction, interpret, set_serial_port, send
+from .mapping import instruction, interpret, send, remote
 
 # Import all sub-modules (forces instructions to register)
 from . import system
