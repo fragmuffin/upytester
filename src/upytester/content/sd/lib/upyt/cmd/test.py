7a144a6cc4ebc89bebd1992a7f507963de4cea46
Peter Boin <peter@allumeenergy.com.au>
2019-11-12 17:28:11 +1100

    docs

--- src/upytester/content/sd/lib/upyt/cmd/test.py
+++ src/upytester/content/sd/lib/upyt/cmd/test.py
@@ -5,10 +5,12 @@ from .mapping import instruction, send
 
 @instruction
 def ping(value=0):
+    """Return given value + 1, must be an integer."""
     return {'value': value + 1}
 
 
 @instruction
 def get_switch():
+    """Onboard switch value."""
     switch_obj = pyb.Switch()
     return {'value': switch_obj.value()}
