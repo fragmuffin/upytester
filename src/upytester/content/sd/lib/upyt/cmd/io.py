b33a48127837b2532b99ac5c8e18110566d695dd
Peter Boin <peter@allumeenergy.com.au>
2019-11-13 13:08:06 +1100

    io.pin instructions down to 1 remote class

--- src/upytester/content/sd/lib/upyt/cmd/io.py
+++ src/upytester/content/sd/lib/upyt/cmd/io.py
@@ -1,88 +1,70 @@
 import machine
 import pyb
 
-from .mapping import instruction, send
+from .mapping import instruction
+from .mapping import remote
 
 
-# ------- Map(s)
-pin_map = {}
+@remote
+class Pin(pyb.Pin):
+    """Remote pin control."""
 
-# ------- Configure
+    def __init__(self, id, mode, pull=None, value=None):
+        """
+        Pin Constructor.
 
-@instruction
-def config_pin(id, mode, pull=None, value=None):
-    r"""
-    :param id: name of pin (eg: ``SW``, ``X7``)
-    :type id: :class:`str`
-    :param mode: pin mode (see table below)
-    :type mode: :class:`str`
-    :param pull: pull up/down resistor (if any), (see table below)
-    :type pull: :class:`str`
-    :param value: if pin is an output, initial value can be set
-    :type value: :class:`int`
+        :param id: name of pin (eg: ``SW``, ``X7``)
+        :type id: :class:`str`
+        :param mode: pin mode (see table below)
+        :type mode: :class:`str`
+        :param pull: pull up/down resistor (if any), (see table below)
+        :type pull: :class:`str`
+        :param value: if pin is an output, initial value can be set
+        :type value: :class:`int`
 
-    **Parameter:** ``mode``
+        **Parameter:** ``mode``
 
-    ==============  ======================================
-    ``mode`` Value  Pin Behaviour
-    ==============  ======================================
-    ``'in'``        input
-    ``'out'``       (synonymn for 'out_pp')
-    ``'out_pp'``    output, with push-pull control
-    ``'out_od'``    output, with open-drain control
-    ``'af_pp'``     alternate function, pull-pull
-    ``'af_od'``     alternate function, open-drain
-    ``'analog'``    analog
-    ==============  ======================================
+        ==============  ======================================
+        ``mode`` Value  Pin Behaviour
+        ==============  ======================================
+        ``'in'``        input
+        ``'out'``       (synonymn for 'out_pp')
+        ``'out_pp'``    output, with push-pull control
+        ``'out_od'``    output, with open-drain control
+        ``'af_pp'``     alternate function, pull-pull
+        ``'af_od'``     alternate function, open-drain
+        ``'analog'``    analog
+        ==============  ======================================
 
-    **Parameter:** ``pull``
+        **Parameter:** ``pull``
 
-    ==============  =========================
-    ``pull`` Value  Pin Behaviour
-    ==============  =========================
-    ``None``        no pull up/down
-    ``'none'``      no pull up/down
-    ``'up'``        pull up resistor
-    ``'down'``      pull down resistor
-    ==============  =========================
-
-    """
-    # Configure pin
-    args = {
-        'mode': {
-            'in': pyb.Pin.IN,  # input
-            'out': pyb.Pin.OUT_PP,  # (synonymn for 'out_pp')
-            'out_pp': pyb.Pin.OUT_PP,  # output, with push-pull control
-            'out_od': pyb.Pin.OUT_OD,  # output, with open-drain control
-            'af_pp': pyb.Pin.AF_PP,  # alternate function, pull-pull
-            'af_od': pyb.Pin.AF_OD,  # alternate function, open-drain
-            'analog': pyb.Pin.ANALOG,  # analog
-        }[mode],
-        'pull': {
-            None: pyb.Pin.PULL_NONE,
-            'none': pyb.Pin.PULL_NONE,
-            'up': pyb.Pin.PULL_UP,
-            'down': pyb.Pin.PULL_DOWN,
-        }[pull],
-    }
-
-    pin = pyb.Pin(id, **args)
-    if value is not None:
-        pin.value(value)
-
-    # Assign pin to map
-    pin_map[id] = pin
-
-
-# ------- Get / Set
-
-@instruction
-def get_pin(id):
-    pin = pin_map[id]
-    return {'pin': id, 'v': pin.value()}
-
-
-@instruction
-def set_pin(id, v):
-    pin = pin_map[id]
-    pin.value(v)
+        ==============  =========================
+        ``pull`` Value  Pin Behaviour
+        ==============  =========================
+        ``None``        no pull up/down
+        ``'none'``      no pull up/down
+        ``'up'``        pull up resistor
+        ``'down'``      pull down resistor
+        ==============  =========================
+        """
+        super().__init__(id)
+        self.init(**{
+            'mode': {
+                'in': pyb.Pin.IN,  # input
+                'out': pyb.Pin.OUT,  # (synonymn for 'out_pp')
+                'out_pp': pyb.Pin.OUT_PP,  # output, with push-pull control
+                'out_od': pyb.Pin.OUT_OD,  # output, with open-drain control
+                'af_pp': pyb.Pin.AF_PP,  # alternate function, pull-pull
+                'af_od': pyb.Pin.AF_OD,  # alternate function, open-drain
+                'analog': pyb.Pin.ANALOG,  # analog
+            }[mode],
+            'pull': {
+                None: pyb.Pin.PULL_NONE,
+                'none': pyb.Pin.PULL_NONE,
+                'up': pyb.Pin.PULL_UP,
+                'down': pyb.Pin.PULL_DOWN,
+            }[pull],
+        })
+        # Set initial value (if given)
+        if value is not None:
+            self.value(value)
